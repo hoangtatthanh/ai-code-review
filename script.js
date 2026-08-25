8963d7693cb327c3c9759be3d4286d2f9b1ac1b5
thanhht <thanhht@vtc.vn>
2026-08-25 14:17:37 +0700

    ưpdate

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var button = document.getElementById('buy-btn');
 var unusedVariable = "Hello n8n!";
 
 button.addEventListener('click', function() {
+    // Để quên console.log trong code production
     console.log("Người dùng đã click vào nút mua hàng");
     alert('Cảm ơn bạn đã quan tâm đến sản phẩm của TimberTusk!');
 });
